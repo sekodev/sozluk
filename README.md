61cb9647d60905b63903c1c1f37cb91f4ab39085
Serkan Aksit <serkanaksit@gmail.com>
2015-11-13 21:00:02 +0200

    Fix default bug and update README
    Fixed a bug where default translation won't load. Also, updated contact
    information and README file

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Sozluk(meaning 'dictionary' in Turkish) is a localization library for Corona SDK
 
 Main difference of this library is that it uses the power of Lua tables (files) to store localization data, instead of text or json files. This will let you easily update your games/apps over 50MB on Google Play without coping with patch files.
 
+If you have questions, feel free to send me a tweet -> https://twitter.com/sekodev
+
 
 ##Basic instructions
 **Import library**
--- Sozluk.lua
+++ Sozluk.lua
@@ -24,17 +24,18 @@
 
 -- Project: Sozluk
 --
--- Date: April 15, 2015
+-- Date: November 13, 2015
 --
--- Version: 0.1
+-- Version: 0.1.1
 --
 -- Author: Serkan Aksit -> https://github.com/sekodev -> https://twitter.com/sekodev
--- Sleepy Bug Studios -> http://sleepybugstudios.com -> https://www.facebook.com/sleepybugstudios
+-- Sleepy Bug Studios -> http://www.sleepybug.xyz -> https://www.facebook.com/sleepybugstudios
 --
 -- Tools: Sublime Text 3 with Corona Editor 1.5.1
 --
 -- Update History:
 -- 0.1 - Initial release
+-- 0.1.1 - Fixed a bug where default translation won't load
 
 -- ########################################################
 -- Basic instructions
